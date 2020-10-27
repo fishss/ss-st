890549d0484f52edc61343c34d6be2a35269d07e
Shaufi Shamsul <fishss@protonmail.com>
2020-10-27 11:59:54 +0800

    Selection as clipboard

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liga MonoStroom:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Liga InconsolataForPowerlineMod:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=14:antialias=true:autohint=true", 
